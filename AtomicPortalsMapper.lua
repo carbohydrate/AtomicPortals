75999dedd3ee1358b7fa9b6a47252ed7dc3d96a7
Kyle Fletcher <kfletcher87@gmail.com>
2026-04-01 11:34:33 -0500

    midnight season 1

--- AtomicPortalsMapper.lua
+++ AtomicPortalsMapper.lua
@@ -10,7 +10,23 @@ local function getMotherlodeId()
     end
 end
 
+-- https://wago.tools/db2/MapChallengeMode
 local spellTable = {
+    -- The Burning Crusade
+
+    -- Wrath of the Lich King
+    [556] = { spellId = 1254555, sName = "POS" },
+
+    -- Cataclysm
+
+    -- Mists of Pandaria
+
+    -- Warlords of Draenor
+    [161] = { spellId = 159898, sName = "SR" },
+
+    -- Legion
+    [239] = { spellId = 1254551, sName = "SEAT" },
+
     -- Battle for Azeroth
     [247] = { spellId = getMotherlodeId(), sName = "ML" },
 
@@ -18,6 +34,9 @@ local spellTable = {
     [378] = { spellId = 354465, sName = "HOA" },
     [392] = { spellId = 367416, sName = "GMBT" },
     [391] = { spellId = 367416, sName = "STRT" },
+
+    -- Dragonflight
+    [402] = { spellId = 393273, sName = "AA" },
 
     -- The War Within
     [503] = { spellId = 445417, sName = "ARAK" },
@@ -30,6 +49,12 @@ local spellTable = {
     [499] = { spellId = 445444, sName = "PSF" },
     [500] = { spellId = 445443, sName = "ROOK" },
     [501] = { spellId = 445269, sName = "SV" },
+
+    -- Midnight
+    [560] = { spellId = 1254559, sName = "MC" },
+    [558] = { spellId = 1254572, sName = "MT" },
+    [559] = { spellId = 1254563, sName = "NPX" },
+    [557] = { spellId = 1254400, sName = "WS" },
 }
 
 local function getSpellInfoFromMapId(mapId)
